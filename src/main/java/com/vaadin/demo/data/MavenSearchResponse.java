e1fc9d9178c7c969d12037c3032c5eef6df759b6
Teemu Pöntelin <teemu@vaadin.com>
2015-02-06 11:53:35 +0200

    Ignore "alternate" field

--- src/main/java/com/vaadin/demo/data/MavenSearchResponse.java
+++ src/main/java/com/vaadin/demo/data/MavenSearchResponse.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties({ "status", "spellcheck" })
+@JsonIgnoreProperties({ "status", "spellcheck", "alternate" })
 public class MavenSearchResponse {
 
     private ResponseHeader responseHeader;
